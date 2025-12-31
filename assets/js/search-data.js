02b01fa900f7fd5d0a19f1b6783103bbfe5d8b3b
moistym <135056638+moistym@users.noreply.github.com>
2025-12-31 04:05:31 +0000

    Deploying to gh-pages from @ moistym/moistym.github.io@c42b42ba4b2377e7374a328c5f198dc94d8d5a38 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -43,9 +43,9 @@ ninja.data = [{
           description: "Experimental validation of the inverse-square law and Lambert’s cosine law using Arduino sensing and MATLAB analysis.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-attitude-dynamics-and-control-of-a-rigid-spacecraft-in-leo",
+          title: 'Attitude Dynamics and Control of a Rigid Spacecraft in LEO',
+          description: "Quaternion simulation, gravity-gradient torque, and PD stabilization in MATLAB.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
