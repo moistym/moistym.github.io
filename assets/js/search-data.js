9615559e7d79c8c2d8f115d3ac649743c3ab1173
moistym <135056638+moistym@users.noreply.github.com>
2026-03-13 21:35:31 +0000

    Deploying to gh-pages from @ moistym/moistym.github.io@f3cf215d73965e3169f7542a4647b5e782205e16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,6 +38,11 @@ ninja.data = [{
           description: "MATLAB-based rigid-body flight dynamics model capturing full translational and rotational aircraft motion",
           section: "Projects",handler: () => {
               window.location.href = "/projects/11_project/";
+            },},{id: "projects-esa-spark-2026-spacecraft-detection-and-segmentation",
+          title: 'ESA SPARK 2026 – Spacecraft Detection and Segmentation',
+          description: "Deep learning pipeline for spacecraft classification and semantic segmentation in synthetic orbital imagery",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/12_project/";
             },},{id: "projects-orbitflex-cubesat-team",
           title: 'OrbitFlex CubeSat Team',
           description: "3U CubeSat optical SSA mission for GEO line-of-sight measurement collection.",
